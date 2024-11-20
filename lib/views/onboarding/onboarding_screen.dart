16ce9cb48d64d99d34280238ec4e94805a9551cd
Shahzain Ahmed <shahzainahmed57@gmail.com>
2024-11-20 12:04:51 +0500

    [ADD] Added a bottom container widget

--- lib/views/onboarding/onboarding_screen.dart
+++ lib/views/onboarding/onboarding_screen.dart
@@ -1,7 +1,7 @@
 import 'package:bounce/bounce.dart';
 import 'package:coffee_app/resources/app_assets.dart';
 import 'package:coffee_app/resources/app_colors.dart';
-import 'package:coffee_app/resources/app_typography.dart';
+import 'package:coffee_app/views/onboarding/widgets/bottom_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -24,93 +24,7 @@ class OnboardingScreen extends StatelessWidget {
             width: Get.width,
             child: Image.asset(AppAssets.kOnboarding, fit: BoxFit.cover),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: EdgeInsets.only(left: 24.w, right: 70.w, top: 30.h),
-              height: Get.height * 0.45,
-              width: Get.width,
-              decoration: const BoxDecoration(
-                color: AppColors.kOnboardingColor,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(35)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Get A",
-                        style: AppTypography.kExtraBold46
-                            .copyWith(color: AppColors.kWhiteColor),
-                      ),
-                      Container(
-                        height: 30.h,
-                        width: 126.w,
-                        decoration: BoxDecoration(
-                          color: AppColors.kBrownColor,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.r),
-                            topRight: Radius.circular(50.r),
-                            bottomLeft: Radius.circular(30.r),
-                            bottomRight: Radius.circular(14.r),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 30.h,
-                        width: 80.w,
-                        decoration: BoxDecoration(
-                          color: AppColors.kBrownColor,
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
-                      ),
-                      Text(
-                        "Craving",
-                        style: AppTypography.kExtraBold46
-                            .copyWith(color: AppColors.kWhiteColor),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Grab A",
-                        style: AppTypography.kExtraBold46
-                            .copyWith(color: AppColors.kWhiteColor),
-                      ),
-                      Container(
-                        height: 30.h,
-                        width: 100.w,
-                        decoration: BoxDecoration(
-                          color: AppColors.kBrownColor,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.r),
-                            topRight: Radius.circular(50.r),
-                            bottomLeft: Radius.circular(30.r),
-                            bottomRight: Radius.circular(10.r),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    "Cookie!",
-                    style: AppTypography.kExtraBold46
-                        .copyWith(color: AppColors.kWhiteColor),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          const BottomContainer(),
           Positioned(
             right: -22,
             bottom: -40,
