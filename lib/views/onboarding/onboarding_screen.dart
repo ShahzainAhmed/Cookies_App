9101502e72db6e81582f4293a41586ac3ebebc3d
Shahzain Ahmed <shahzainahmed57@gmail.com>
2024-11-20 12:11:47 +0500

    [FIX] Fixed  buttons size on the bottom container on onboarding screen.

--- lib/views/onboarding/onboarding_screen.dart
+++ lib/views/onboarding/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bounce/bounce.dart';
 import 'package:coffee_app/resources/app_assets.dart';
 import 'package:coffee_app/resources/app_colors.dart';
+import 'package:coffee_app/views/home/home_screen.dart';
 import 'package:coffee_app/views/onboarding/widgets/bottom_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -26,26 +27,29 @@ class OnboardingScreen extends StatelessWidget {
           ),
           const BottomContainer(),
           Positioned(
-            right: -22,
-            bottom: -40,
-            child: Bounce(
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: AppColors.kBrownColor.withOpacity(0.5),
-                    radius: 80.r,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: AppColors.kLightBrownColor,
-                    radius: 70.r,
-                    child: const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      color: AppColors.kWhiteColor,
-                      size: 36,
+            right: -26,
+            bottom: -46,
+            child: GestureDetector(
+              onTap: () => Get.to(const HomeScreen()),
+              child: Bounce(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: AppColors.kBrownColor.withOpacity(0.5),
+                      radius: 84.r,
                     ),
-                  ),
-                ],
+                    CircleAvatar(
+                      backgroundColor: AppColors.kLightBrownColor,
+                      radius: 70.r,
+                      child: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: AppColors.kWhiteColor,
+                        size: 36,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
